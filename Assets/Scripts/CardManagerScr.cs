78c1492ab8e4fc17effa7421859bddfa830e83ea
Nelazsky <nelazsky.vladimir@gmail.com>
2021-01-08 20:13:05 +0300

    Initial commit

---
+++ Assets/Scripts/CardManagerScr.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Card
+{
+    public string Name;
+    public Sprite Logo;
+    public int Attack, Defense;
+
+    public Card(string name, string logoPath, int attack, int defense)
+    {
+        Name = name;
+        Logo = Resources.Load<Sprite>(logoPath);
+        Attack = attack;
+        Defense = defense;
+
+    }
+}
+
+public static class CardManager
+{
+    public static List<Card> AllCards = new List<Card>();
+}
+public class CardManagerScr : MonoBehaviour
+{
+    public void Awake()
+    {
+        CardManager.AllCards.Add(new Card("Airat", "Sprites/Cards/1", 5, 5));
+        CardManager.AllCards.Add(new Card("Misha", "Sprites/Cards/2", 15, 15));
+        CardManager.AllCards.Add(new Card("Rustem", "Sprites/Cards/3", 5, 5));
+        CardManager.AllCards.Add(new Card("Ruslan", "Sprites/Cards/4", 5, 5));
+        CardManager.AllCards.Add(new Card("Vova", "Sprites/Cards/5", 1, 1));
+        CardManager.AllCards.Add(new Card("Dima", "Sprites/Cards/6", 10, 10));
+        CardManager.AllCards.Add(new Card("Rifkat", "Sprites/Cards/7", 5, 5));
+    }
+}
---
+++ Assets/Scripts/DropPlaceScr.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum FieldType
+{
+    SELF_HAND,
+    SELF_FIELD,
+    ENEMY_HAND,
+    ENEMY_FIELD
+}
+
+public class DropPlaceScr : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public FieldType Type;
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(Type != FieldType.SELF_FIELD)
+            return;
+        
+        
+        CardMovementScr card = eventData.pointerDrag.GetComponent<CardMovementScr>();
+
+        if (card)
+        {
+            card.DefaultParent = transform;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag == null || Type == FieldType.ENEMY_FIELD || Type == FieldType.ENEMY_HAND)
+            return;
+
+        CardMovementScr card = eventData.pointerDrag.GetComponent<CardMovementScr>();
+
+        if (card)
+            card.DefaultTempCardParent = transform;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag == null)
+            return;
+        
+        CardMovementScr card = eventData.pointerDrag.GetComponent<CardMovementScr>();
+
+        if (card && card.DefaultTempCardParent == transform)
+            card.DefaultTempCardParent = card.DefaultParent;
+
+    }
+}
